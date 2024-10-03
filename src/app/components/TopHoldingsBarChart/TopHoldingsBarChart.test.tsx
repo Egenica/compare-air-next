a0543b2a5529ee0ae68d8f18c4b7b879d9f30f36
JKL <16577036+Egenica@users.noreply.github.com>
2024-10-03 01:36:28 +0100

    swapped bar chart

--- src/app/components/TopHoldingsBarChart/TopHoldingsBarChart.test.tsx
+++ src/app/components/TopHoldingsBarChart/TopHoldingsBarChart.test.tsx
@@ -1,47 +1,25 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { TopHoldingsBarChart } from './TopHoldingsBarChart';
-import 'jest-canvas-mock';
+
+const mockHoldings = [
+  { name: 'Apple', weighting: 30 },
+  { name: 'Microsoft', weighting: 20 },
+  { name: 'Google', weighting: 15 },
+  { name: 'Amazon', weighting: 25 },
+  { name: 'Facebook', weighting: 10 },
+];
 
 describe('TopHoldingsBarChart', () => {
-  const mockHoldings = [
-    { name: 'Apple', weighting: 30 },
-    { name: 'Microsoft', weighting: 25 },
-    { name: 'Google', weighting: 20 },
-    { name: 'Amazon', weighting: 15 },
-    { name: 'Facebook', weighting: 10 },
-  ];
-
   it('renders without crashing', async () => {
     render(<TopHoldingsBarChart holdings={mockHoldings} />);
   });
 
   it('displays the chart', async () => {
     render(<TopHoldingsBarChart holdings={mockHoldings} />);
-    const bars = screen.getAllByRole('img', { hidden: true });
-    expect(bars.length).toBe(1);
-  });
-
-  it('should return the correct name from holdings based on dataIndex', () => {
-    const holdings = [
-      { name: 'Holding 1', weighting: 10 },
-      { name: 'Holding 2', weighting: 20 },
-      { name: 'Holding 3', weighting: 30 },
-    ];
-
-    const tooltipItems = [{ dataIndex: 1 }];
-
-    // Mock the Bar component to access the options
-    jest.mock('react-chartjs-2', () => ({
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      Bar: ({ options }: { options: any }) => {
-        const titleCallback = options.plugins.tooltip.callbacks.title;
-        expect(titleCallback(tooltipItems)).toBe('Holding 2');
-        return null;
-      },
-    }));
-
-    render(<TopHoldingsBarChart holdings={holdings} />);
+    const chart = screen.getByTestId('top-holdings-bar-chart');
+    const div = await waitFor(() => chart.querySelector('div'));
+    expect(div).toBeInTheDocument();
   });
 });
--- src/app/components/TopHoldingsBarChart/TopHoldingsBarChart.tsx
+++ src/app/components/TopHoldingsBarChart/TopHoldingsBarChart.tsx
@@ -1,74 +1,81 @@
-import { Bar } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar';
 
 export const TopHoldingsBarChart = ({
   holdings,
 }: {
   holdings: { name: string; weighting: number }[];
 }) => {
-  const data = {
-    labels: holdings.map((holding) => holding.name.split(' ')[0]),
-    datasets: [
-      {
-        label: 'Weighting (%)',
-        data: holdings.map((holding) => holding.weighting),
-        backgroundColor: 'rgba(54, 162, 235, 0.7)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
+  const data = holdings.map((holding) => {
+    const [firstName, lastName] = holding.name.split(' ');
+    return {
+      name: `${firstName}-${lastName}`, // Optional adjustment for name
+      weighting: holding.weighting,
+    };
+  });
 
-  const options = {
-    indexAxis: 'y' as const,
-    responsive: true,
-    maintainAspectRatio: true,
-    aspectRatio: 1.2,
-    scales: {
-      x: {
-        ticks: {
-          autoSkip: false,
-          maxTicksLimit: 10,
-          minRotation: 0,
-          maxRotation: 0,
-        },
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        callbacks: {
-          title: (tooltipItems: { dataIndex: number }[]) => {
-            const index = tooltipItems[0].dataIndex;
-            return holdings[index].name;
-          },
-        },
-      },
-    },
-  };
+  // Check data.name doesn't have duplicates and if it does add a number to the end of the name
+  const nameCount = new Map();
+  data.forEach((item) => {
+    if (nameCount.has(item.name)) {
+      const count = nameCount.get(item.name);
+      item.name = `${item.name}[${count}]`;
+      nameCount.set(item.name, count + 1);
+    } else {
+      nameCount.set(item.name, 1);
+    }
+  });
+  console.log(data);
+
+  if (!data || data.length === 0) {
+    return <div>No data available</div>;
+  }
 
   return (
-    <div className="mt-4">
-      <Bar data={data} options={options} />
+    <div style={{ aspectRatio: 3 / 3 }} data-testid="top-holdings-bar-chart">
+      <ResponsiveBar
+        data={data}
+        keys={['weighting']}
+        indexBy="name"
+        margin={{ top: 0, right: 0, bottom: 20, left: 100 }}
+        padding={0.4}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={'#79bff1'}
+        borderColor={{ from: 'color', modifiers: [['darker', 0.3]] }}
+        borderWidth={1}
+        axisTop={null}
+        axisRight={null}
+        labelSkipWidth={20}
+        labelSkipHeight={20}
+        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        legends={[
+          {
+            dataFrom: 'keys',
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 120,
+            translateY: 0,
+            itemsSpacing: 2,
+            itemWidth: 100,
+            itemHeight: 20,
+            itemDirection: 'left-to-right',
+            itemOpacity: 0.85,
+            symbolSize: 20,
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+        animate={true}
+        layout="horizontal"
+      />
     </div>
   );
 };
