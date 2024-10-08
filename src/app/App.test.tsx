0138a166823c644d56a9de94873096b5e311d8de
JKL <16577036+Egenica@users.noreply.github.com>
2024-10-08 20:31:39 +0100

    Update App.test.tsx

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
 // teardown and cleanup
@@ -10,25 +10,35 @@ afterEach(() => {
 window.scrollTo = jest.fn();
 
 test('Renders loading text', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const el = screen.getByText(/LOADING DATA/i);
   expect(el).toBeInTheDocument();
 });
 
 test('Renders heading text', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const el = screen.getByText(/Compare your Air/i);
   expect(el).toBeInTheDocument();
 });
 
 test('Renders subheading text', () => {
-  render(<App />);
-  const el = screen.getByText(/Compare the air quality between cities in the UK/i);
+  act(() => {
+    render(<App />);
+  });
+  const el = screen.getByText(
+    /Compare the air quality between cities in the UK/i
+  );
   expect(el).toBeInTheDocument();
 });
 
 test('Renders to top of page button and tests click', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const el = screen.getByText(/TOP/i);
 
   expect(el).toBeInTheDocument();
