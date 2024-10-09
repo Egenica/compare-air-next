0222fcbb598f50dcd0ac5b791245231d7e76408f
JKL <16577036+Egenica@users.noreply.github.com>
2024-10-09 19:51:30 +0100

    test updates

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
 // teardown and cleanup
@@ -11,28 +11,27 @@ window.scrollTo = jest.fn();
 
 describe('App Component', () => {
   it('should render without crashing', async () => {
-    const appComp = await App();
-
-    const { container } = render(appComp);
-
-    await expect(container).toBeInTheDocument();
+    await act(async () => {
+      const appComp = await App();
+      const { container } = render(appComp);
+      await expect(container).toBeInTheDocument();
+    });
   });
 
-  it('should render the App component', async () => {
-    const appComp = await App();
-    const { getByText } = render(appComp);
-    expect(getByText('Compare your Air')).toBeInTheDocument();
-  });
-
-  test('Renders loading text', async () => {
-    const appComp = await App();
-    const { getByText } = render(appComp);
-    expect(getByText(/LOADING DATA/i)).toBeInTheDocument();
+  test('should render the App component', async () => {
+    await act(async () => {
+      const appComp = await App();
+      render(appComp);
+    });
+    const el = screen.getByText(/Compare your Air/i);
+    expect(el).toBeInTheDocument();
   });
 
   test('Renders subheading text', async () => {
-    const appComp = await App();
-    render(appComp);
+    await act(async () => {
+      const appComp = await App();
+      render(appComp);
+    });
     const el = screen.getByText(
       /Compare the air quality between cities in the UK/i
     );
@@ -40,9 +39,10 @@ describe('App Component', () => {
   });
 
   test('Renders to top of page button and tests click', async () => {
-    const appComp = await App();
-    render(appComp);
-
+    await act(async () => {
+      const appComp = await App();
+      render(appComp);
+    });
     const el = screen.getByText(/TOP/i);
 
     expect(el).toBeInTheDocument();
--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,20 +1,13 @@
 import '@testing-library/jest-dom';
-//import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import App from './App';
 
 describe('Home Component', () => {
   it('should render without crashing', async () => {
-    const appComp = await App();
-
-    const { container } = render(appComp);
-
-    await expect(container).toBeInTheDocument();
-  });
-
-  it('should render the App component', async () => {
-    const appComp = await App();
-    const { getByText } = render(appComp);
-    expect(getByText('Compare your Air')).toBeInTheDocument();
+    await act(async () => {
+      const appComp = await App();
+      const { container } = render(appComp);
+      await expect(container).toBeInTheDocument();
+    });
   });
 });
